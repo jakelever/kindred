f50b0ade3be9e560aea5f8aaee248df5d9514ca2
Jake Lever <jlever@bcgsc.ca>
2017-04-18 13:11:44 -0700

    Forgot datatypes test for datageneration change

--- tests/test_datatypes.py
+++ tests/test_datatypes.py
@@ -5,7 +5,7 @@ from kindred.Parser import Parser
 from kindred.RelationClassifier import RelationClassifier
 from kindred.Evaluator import Evaluator
 
-from datageneration import *
+from kindred.datageneration import generateData,generateTestData
 
 def test_convertTaggedText():
 	#text = 'The <drug><disease>Erlotinib</disease></drug> is a common treatment for <cancer>NSCLC</cancer> patients'
