53ffc413e5c5ad47f78550becc40e8ffe738e1bc
Jake Lever <jlever@bcgsc.ca>
2017-04-18 22:13:02 -0700

    Added broken tests back in with specific ones for the paper (all fail)

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -7,32 +7,32 @@ from kindred.Evaluator import Evaluator
 
 from kindred.datageneration import generateData,generateTestData
 
-#def test_bionlpst():
-#	trainData = kindred.BioNLPSTData('2016-BB3-event-training')
-#	devData = kindred.BioNLPSTData('2016-BB3-event-development')
-#	model = kindred.train(trainData)
-#	predictedRelations = model.predict(dev_data.getTextAndEntities())
-#	f1score = kindred.evaluate(dev_data.getRelations(), predictionRelations, metric='f1score')
-#	assert f1score > 0.5
+def test_bionlpst():
+	trainData = kindred.BioNLPSTData('2016-BB3-event-training')
+	devData = kindred.BioNLPSTData('2016-BB3-event-development')
+	model = kindred.train(trainData)
+	predictedRelations = model.predict(dev_data.getTextAndEntities())
+	f1score = kindred.evaluate(dev_data.getRelations(), predictionRelations, metric='f1score')
+	assert f1score > 0.5
 
-#def test_pubannotation():
-#	trainData = kindred.PubAnnotationData('2016-SeeDev-binary-training')
-#	model = kindred.train(trainData)
-#	text = 'A SeeDev related text goes here'
-#	predictedRelations = model.predict(text)
-#	assert len(predicted_relations) == 1
+def test_pubannotation():
+	trainData = kindred.PubAnnotationData('2016-SeeDev-binary-training')
+	model = kindred.train(trainData)
+	text = 'A SeeDev related text goes here'
+	predictedRelations = model.predict(text)
+	assert len(predicted_relations) == 1
 
-#def test_pubmed():
-#	assert False
+def test_pubtator():
+	assert False
 	
-#def test_unicodeCheck():
-#	assert False
+def test_unicodeCheck():
+	assert False
 	
-#def test_exportToST():
-#	assert False
+def test_exportToST():
+	assert False
 
-#def test_naryRelations():
-#	assert False
+def test_naryRelations():
+	assert False
 	
 if __name__ == '__main__':
 	test_simpleRelationCheck()
---
+++ tests/test_paperfigures.py
@@ -0,0 +1,61 @@
+import kindred
+from kindred.CandidateBuilder import CandidateBuilder
+from kindred.Vectorizer import Vectorizer
+from kindred.Parser import Parser
+from kindred.RelationClassifier import RelationClassifier
+from kindred.Evaluator import Evaluator
+
+from kindred.datageneration import generateData,generateTestData
+
+def test_bionlpst_bb3():	
+	trainData = kindred.BioNLPSTData('2016-BB3-event-training_and_development')
+	testData = kindred.BioNLPSTData('2016-BB3-event-test')
+	
+	classifier = RelationClassifier()
+	classifier.train(trainData)
+	
+	predictedRelations = classifier.predict(testData)
+	
+	print classifier.getCrossvalidatedScore()
+	kindred.saveST('BB3-predictions/',testData,predictedRelations)
+	
+def test_bionlpst_seedev():	
+	trainData = kindred.BioNLPSTData('2016-SeeDev-binary-training_and_development')
+	testData = kindred.BioNLPSTData('2016-SeeDev-binary-test')
+	
+	classifier = RelationClassifier()
+	classifier.train(trainData)
+	
+	predictedRelations = classifier.predict(testData)
+	
+	print classifier.getCrossvalidatedScore()
+	kindred.saveST('BB3-predictions/',testData,predictedRelations)
+	
+def test_nary():	
+	trainData = kindred.BioNLPSTData('2016-SeeDev-binary-training_and_development')
+	testData = kindred.BioNLPSTData('2016-SeeDev-binary-test')
+	
+	classifier_merge = RelationClassifier(naryMethod='merge')
+	classifier_merge.train(trainData)
+	predictedRelations_merge = classifier_merge.predict(testData)
+	kindred.saveST('BB3-predictions-merge/',testData,predictedRelations_merge)
+	
+	classifier_combined = RelationClassifier(naryMethod='combine')
+	classifier_combined.train(trainData)
+	predictedRelations_combined = classifier_combined.predict(testData)
+	kindred.saveST('BB3-predictions-combined/',testData,predictedRelations_combined)
+	
+def test_parser():	
+	trainData = kindred.BioNLPSTData('2016-SeeDev-binary-training_and_development')
+	testData = kindred.BioNLPSTData('2016-SeeDev-binary-test')
+	
+	classifier_stanford = RelationClassifier(parser='stanford')
+	classifier_stanford.train(trainData)
+	predictedRelations_stanford = classifier_stanford.predict(testData)
+	kindred.saveST('BB3-predictions-stanford/',testData,predictedRelations_stanford)
+	
+	classifier_malt = RelationClassifier(parser='malt')
+	classifier_malt.train(trainData)
+	predictedRelations_malt = classifier_malt.predict(testData)
+	kindred.saveST('BB3-predictions-malt/',testData,predictedRelations_malt)
+	
