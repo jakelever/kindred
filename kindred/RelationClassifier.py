61d645d25606680efab7b2681077b4dbc0745b6e
Jake Lever <jlever@bcgsc.ca>
2017-04-21 18:39:16 -0700

    Gave classifier some options for single/multi classifiers and feature building

--- kindred/RelationClassifier.py
+++ kindred/RelationClassifier.py
@@ -1,4 +1,6 @@
 
+import sys
+
 from sklearn import svm
 import numpy as np
 
@@ -42,6 +44,53 @@ class RelationClassifier:
 		Constructor-time
 		"""
 		self.isTrained = False
+		self.useSingleClassifier = True
+		self.useBuilder = False
+
+		self.defaultFeatures = ["selectedTokenTypes","ngrams_betweenEntities","bigrams","dependencyPathElements","dependencyPathNearSelected"]
+
+	def buildFeatureSet(self,candidateRelations,classes):
+		vectorizers = {}
+		trainVectors = {}
+
+		featureChoice = ["selectedTokenTypes","dependencyPathElements","ngrams_betweenEntities","bigrams_betweenEntities","bigramsOfDependencyPath"]
+		for feature in featureChoice:
+			vectorizers[feature] = Vectorizer()
+			trainVectors[feature] = vectorizers[feature].transform(candidateRelations,[feature])
+
+		groupVector = None
+		chosenFeatures = []
+		prevScore,prevMatrix = -1.0, None
+		while True:
+			bestScore, bestFeature, bestMatrix = -1.0, None, None
+			for feature in featureChoice:
+				if prevMatrix is None:
+					matrix = trainVectors[feature]
+				else:
+					matrix = hstack([prevMatrix,trainVectors[feature]])
+
+				clf = svm.LinearSVC(class_weight='balanced')
+				
+				scores = cross_val_score(clf, matrix, classes, cv=5, scoring='f1_macro')
+				score = scores.mean()
+
+				#print chosenFeatures, feature, score, scores
+				if score > bestScore:
+					bestScore = score
+					bestFeature = feature
+					bestMatrix = matrix
+
+			if bestScore > prevScore:
+				# We see improvement
+				featureChoice.remove(bestFeature)
+				chosenFeatures.append(bestFeature)
+				prevScore = bestScore
+				prevMatrix = bestMatrix
+			else:
+				# No improvement made
+				break
+
+		return chosenFeatures
 
 	def train(self,data):
 		"""
@@ -68,68 +117,62 @@ class RelationClassifier:
 
 		# We'll just get the vectors for the selectedTokenTypes
 
-		tmpClassData = [ (1 in candidateClassGroup) for candidateClassGroup in candidateClasses ]
+		#tmpClassData = [ (1 in candidateClassGroup) for candidateClassGroup in candidateClasses ]
 
-		vectorizers = {}
-		trainVectors = {}
+		#useSingleClassifier = False
+		tfidf = True
+		if self.useSingleClassifier:
+			#chosenFeatures = ["selectedTokenTypes","dependencyPathElements","ngrams_betweenEntities","bigrams_betweenEntities","bigramsOfDependencyPath"]
 
-		featureChoice = ["selectedTokenTypes","dependencyPathElements","ngrams_betweenEntities","bigrams_betweenEntities","bigramsOfDependencyPath"]
-		for feature in featureChoice:
-			vectorizers[feature] = Vectorizer()
-			trainVectors[feature] = vectorizers[feature].transform(candidateRelations,[feature])
 
-		groupVector = None
-		chosenFeatures = []
-		prevScore,prevMatrix = -1.0, None
-		while True:
-			bestScore, bestFeature, bestMatrix = -1.0, None, None
-			for feature in featureChoice:
-				if prevMatrix is None:
-					matrix = trainVectors[feature]
-				else:
-					matrix = hstack([prevMatrix,trainVectors[feature]])
 
-				clf = svm.LinearSVC(class_weight='balanced')
-				
-				scores = cross_val_score(clf, matrix, tmpClassData, cv=5, scoring='f1_macro')
-				score = scores.mean()
-
-				print chosenFeatures, feature, score, scores
-				if score > bestScore:
-					bestScore = score
-					bestFeature = feature
-					bestMatrix = matrix
-
-			if bestScore > prevScore:
-				# We see improvement
-				featureChoice.remove(bestFeature)
-				chosenFeatures.append(bestFeature)
-				prevScore = bestScore
-				prevMatrix = bestMatrix
-			else:
-				# No improvement made
-				break
-				
-		print chosenFeatures
-		#assert False
-		self.vectorizer = Vectorizer()
-		trainVectors = self.vectorizer.transform(candidateRelations,featureChoice=chosenFeatures)
+			simplifiedClasses = []
+			# TODO: Try sparse matrix rep
+			for candidateRelation,candidateClassGroup in zip(candidateRelations,candidateClasses):
+				#assert len(candidateClassGroup) == 1, "Cannot handle multiple relations with same set of entities " + str(candidateRelation)
+				simplifiedClasses.append(candidateClassGroup[0])
+			#print chosenFeatures
+			#assert False
 	
-		print trainVectors.shape
-		assert trainVectors.shape[0] == len(candidateClasses)
-		
-		
-		# TODO: Should we take into account the argument count when grouping classifiers?
-		self.clfs = {}
-		for c in self.allClasses:
-			tmpClassData = [ (c in candidateClassGroup) for candidateClassGroup in candidateClasses ]
+			if self.useBuilder:
+				chosenFeatures = self.buildFeatureSet(candidateRelations,simplifiedClasses)
+			else:
+				chosenFeatures = self.defaultFeatures
 
-			save_sparse_csr('train.matrix',trainVectors.tocsr())
-			saveClasses('train.classes',tmpClassData)
+			self.vectorizer = Vectorizer()
+			trainVectors = self.vectorizer.transform(candidateRelations,featureChoice=chosenFeatures,tfidf=tfidf)
+		
+			print trainVectors.shape
+			assert trainVectors.shape[0] == len(candidateClasses)
+		
+			self.clf = svm.LinearSVC(class_weight='balanced')
+			self.clf.fit(trainVectors,simplifiedClasses)
+		else:
+			# TODO: Should we take into account the argument count when grouping classifiers?
 
-			self.clfs[c] = svm.LinearSVC(class_weight='balanced')
-			#self.clfs[c] = AdaBoostClassifier(n_estimators=2)
-			self.clfs[c].fit(trainVectors,tmpClassData)
+			if not self.useBuilder:
+				chosenFeatures = self.defaultFeatures
+
+				self.vectorizer = Vectorizer()
+				tmpMatrix = self.vectorizer.transform(candidateRelations,featureChoice=chosenFeatures,tfidf=tfidf)
+			self.clfs = {}
+			self.vectorizers = {}
+			for c in self.allClasses:
+				tmpClassData = [ (c in candidateClassGroup) for candidateClassGroup in candidateClasses ]
+
+				if self.useBuilder:
+					chosenFeatures = self.buildFeatureSet(candidateRelations,tmpClassData)
+					print c, chosenFeatures
+					self.vectorizers[c] = Vectorizer()
+					tmpMatrix = self.vectorizers[c].transform(candidateRelations,featureChoice=chosenFeatures,tfidf=tfidf)
+
+				#save_sparse_csr('train.matrix',trainVectors.tocsr())
+				#saveClasses('train.classes',tmpClassData)
+
+				self.clfs[c] = svm.LinearSVC(class_weight='balanced')
+				#self.clfs[c] = AdaBoostClassifier(n_estimators=2)
+				#self.clfs[c].fit(trainVectors,tmpClassData)
+				self.clfs[c].fit(tmpMatrix,tmpClassData)
 		
 		self.isTrained = True
 
@@ -141,25 +184,47 @@ class RelationClassifier:
 			assert isinstance(d,kindred.TextAndEntityData) or isinstance(d,kindred.RelationData)
 			
 		_,candidateRelations,testClasses = self.candidateBuilder.build(data)
-		
-		testVectors = self.vectorizer.transform(candidateRelations)
 
-		save_sparse_csr('test.matrix',testVectors.tocsr())
-
-		tmpClassData = [ (1 in candidateClassGroup) for candidateClassGroup in testClasses ]
-		saveClasses('test.classes',tmpClassData)
+		#if False:
+		#	testVectors = self.vectorizer.transform(candidateRelations)
+		#	tmpClassData = [ (1 in candidateClassGroup) for candidateClassGroup in testClasses ]
+		#	print testVectors.shape
 		
-		print testVectors.shape
+		
+		#save_sparse_csr('test.matrix',testVectors.tocsr())
+		#saveClasses('test.classes',tmpClassData)
+
+		
+
 		
 		predictedRelations = []
 		
-		for c in self.allClasses:
-			predictedClasses = self.clfs[c].predict(testVectors)
+		if self.useSingleClassifier:
+			tmpMatrix = self.vectorizer.transform(candidateRelations)
+
+			#predictedClasses = self.clfs[c].predict(testVectors)
+			predictedClasses = self.clf.predict(tmpMatrix)
 			for predictedClass,candidateRelation in zip(predictedClasses,candidateRelations):
 				if predictedClass != 0:
 					relType,nary = self.classToRelType[predictedClass]
 					predictedRelation = kindred.Relation(relType,list(candidateRelation.entitiesInRelation))
 					predictedRelations.append(predictedRelation)
+		else:
+			if not self.useBuilder:
+				tmpMatrix = self.vectorizer.transform(candidateRelations)
+
+			for c in self.allClasses:
+
+				if self.useBuilder:
+					tmpMatrix = self.vectorizers[c].transform(candidateRelations)
+
+				#predictedClasses = self.clfs[c].predict(testVectors)
+				predicted = self.clfs[c].predict(tmpMatrix)
+				for p,candidateRelation in zip(predicted,candidateRelations):
+					if p != 0:
+						relType,nary = self.classToRelType[c]
+						predictedRelation = kindred.Relation(relType,list(candidateRelation.entitiesInRelation))
+						predictedRelations.append(predictedRelation)
 					
 		return predictedRelations
 					
