747d5ef3ea27c9801c545029a867d941c3206233
Jake Lever <jlever@bcgsc.ca>
2017-04-27 18:08:40 -0700

    Attempt to make BioC Py3-compatible

--- kindred/bioc/__init__.py
+++ kindred/bioc/__init__.py
@@ -12,13 +12,25 @@ __all__ = [
 
 __author__ = 'Hernani Marques (h2m@access.uzh.ch)'
 
-from bioc_annotation import BioCAnnotation
-from bioc_collection import BioCCollection
-from bioc_document import BioCDocument
-from bioc_location import BioCLocation
-from bioc_node import BioCNode
-from bioc_passage import BioCPassage
-from bioc_relation import BioCRelation
-from bioc_sentence import BioCSentence
-from bioc_reader import BioCReader
-from bioc_writer import BioCWriter
+if sys.version_info >= (3, 0):
+	from .bioc_annotation import BioCAnnotation
+	from .bioc_collection import BioCCollection
+	from .bioc_document import BioCDocument
+	from .bioc_location import BioCLocation
+	from .bioc_node import BioCNode
+	from .bioc_passage import BioCPassage
+	from .bioc_relation import BioCRelation
+	from .bioc_sentence import BioCSentence
+	from .bioc_reader import BioCReader
+	from .bioc_writer import BioCWriter
+else:
+	from bioc_annotation import BioCAnnotation
+	from bioc_collection import BioCCollection
+	from bioc_document import BioCDocument
+	from bioc_location import BioCLocation
+	from bioc_node import BioCNode
+	from bioc_passage import BioCPassage
+	from bioc_relation import BioCRelation
+	from bioc_sentence import BioCSentence
+	from bioc_reader import BioCReader
+	from bioc_writer import BioCWriter
