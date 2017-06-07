530edd2da41d54b1f68d3d5981cc6966127d61b9
Jake Lever <jlever@bcgsc.ca>
2017-06-07 16:02:53 -0700

    Trim code in vectorizer

--- kindred/VERSE_vectorizer.py
+++ kindred/VERSE_vectorizer.py
@@ -201,91 +201,21 @@ class VERSEVectorizer:
 			assert self.argCount == len(e.arguments), 'All examples must have the same number of arguments (%d)' % self.argCount
 			
 		data = OrderedDict()
-		if "splitAcrossSentences" in self.chosenFeatures:
-			data["splitAcrossSentences"] = self.doSplitAcrossSentences(examples,featureNamesOnly)
-		if "ngrams" in self.chosenFeatures:
-			data["ngrams"] = self.doNGrams(examples,featureNamesOnly)
-			
-		if "skipgrams_2" in self.chosenFeatures:
-			data["skipgrams_2"] = self.doSkipGrams(examples,2,featureNamesOnly)
-		if "skipgrams_3" in self.chosenFeatures:
-			data["skipgrams_3"] = self.doSkipGrams(examples,3,featureNamesOnly)
-		if "skipgrams_4" in self.chosenFeatures:
-			data["skipgrams_4"] = self.doSkipGrams(examples,4,featureNamesOnly)
-		if "skipgrams_5" in self.chosenFeatures:
-			data["skipgrams_5"] = self.doSkipGrams(examples,5,featureNamesOnly)
-		if "skipgrams_6" in self.chosenFeatures:
-			data["skipgrams_6"] = self.doSkipGrams(examples,6,featureNamesOnly)
-		if "skipgrams_7" in self.chosenFeatures:
-			data["skipgrams_7"] = self.doSkipGrams(examples,7,featureNamesOnly)
-		if "skipgrams_8" in self.chosenFeatures:
-			data["skipgrams_8"] = self.doSkipGrams(examples,8,featureNamesOnly)
-		if "skipgrams_9" in self.chosenFeatures:
-			data["skipgrams_9"] = self.doSkipGrams(examples,9,featureNamesOnly)
-		if "skipgrams_10" in self.chosenFeatures:
-			data["skipgrams_10"] = self.doSkipGrams(examples,10,featureNamesOnly)
-			
-		for j in range(1,10):
-			if ("ngrams_entityWindowLeft_%d" % j) in self.chosenFeatures:
-				for i in range(self.argCount):
-					data[("ngrams_entityWindowLeft_%d_%d" % (j,i))] = self.doNGramsEntityWindowLeft(examples,j,i,featureNamesOnly)
-		for j in range(1,10):
-			if ("ngrams_entityWindowRight_%d" % j) in self.chosenFeatures:
-				for i in range(self.argCount):
-					data[("ngrams_entityWindowRight_%d_%d" % (j,i))] = self.doNGramsEntityWindowRight(examples,j,i,featureNamesOnly)
-		for j in range(1,10):
-			if ("ngrams_entityWindowBoth_%d" % j) in self.chosenFeatures:
-				for i in range(self.argCount):
-					data[("ngrams_entityWindowBoth_%d_%d" % (j,i))] = self.doNGramsEntityWindowBoth(examples,j,i,featureNamesOnly)
-					
-		for j in range(1,10):
-			if ("bigrams_entityWindowLeft_%d" % j) in self.chosenFeatures:
-				for i in range(self.argCount):
-					data[("bigrams_entityWindowLeft_%d_%d" % (j,i))] = self.doBigramsEntityWindowLeft(examples,j,i,featureNamesOnly)
-		for j in range(1,10):
-			if ("bigrams_entityWindowRight_%d" % j) in self.chosenFeatures:
-				for i in range(self.argCount):
-					data[("bigrams_entityWindowRight_%d_%d" % (j,i))] = self.doBigramsEntityWindowRight(examples,j,i,featureNamesOnly)
-		for j in range(1,10):
-			if ("bigrams_entityWindowBoth_%d" % j) in self.chosenFeatures:
-				for i in range(self.argCount):
-					data[("bigrams_entityWindowBoth_%d_%d" % (j,i))] = self.doBigramsEntityWindowBoth(examples,j,i,featureNamesOnly)
-					
 		if "ngrams_betweenEntities" in self.chosenFeatures:
 			data["ngrams_betweenEntities"] = self.doNgramsBetweenEntities(examples,featureNamesOnly)
-		if "bigrams_betweenEntities" in self.chosenFeatures:
-			data["bigrams_betweenEntities"] = self.doBigramsBetweenEntities(examples,featureNamesOnly)
 		
-		if "selectedngrams" in self.chosenFeatures:
-			for i in range(self.argCount):
-				data["selectedngrams_" + str(i)] = self.doNGramsOfArguments(examples,i,featureNamesOnly)
 		#"dependencyPathNearSelected"
 		if "dependencyPathNearSelected" in self.chosenFeatures:
 			for i in range(self.argCount):
 				data["dependencyPathNearSelected_" + str(i)] = self.doDependencyPathNearSelected(examples,i,featureNamesOnly)
 		if "dependencyPathElements" in self.chosenFeatures:
 		  	data["dependencyPathElements"] = self.doDependencyPathElements(examples,featureNamesOnly)
-		if "lemmas" in self.chosenFeatures:
-			data["lemmas"] = self.doLemmas(examples,featureNamesOnly)
-		if "selectedlemmas" in self.chosenFeatures:
-			for i in range(self.argCount):
-				data["selectedlemmas_" + str(i)] = self.doLemmasOfArguments(examples,i,featureNamesOnly)
 		if "bigrams" in self.chosenFeatures:
 			data["bigrams"] = self.doBiGrams(examples,featureNamesOnly)
-		if "ngramsPOS" in self.chosenFeatures:
-			data["ngramsPOS"] = self.doNGramsWithPOS(examples,featureNamesOnly)
-		if "selectedngramsPOS" in self.chosenFeatures:
-			for i in range(self.argCount):
-				data["selectedngramsPOS_" + str(i)] = self.doNGramsOfArgumentsWithPOS(examples,i,featureNamesOnly)
 
 		if "selectedTokenTypes" in self.chosenFeatures:
 			for i in range(self.argCount):
 				data["selectedTokenTypes_" + str(i)] = self.doSelectedTokenTypes(examples,i,featureNamesOnly)		
-
-		if "ngramsOfDependencyPath" in self.chosenFeatures:
-			data["ngramsOfDependencyPath"] = self.doNGramsOfDependencyPath(examples,featureNamesOnly)
-		if "bigramsOfDependencyPath" in self.chosenFeatures:
-			data["bigramsOfDependencyPath"] = self.doBiGramsOfDependencyPath(examples,featureNamesOnly)
 
 		if featureNamesOnly:
 			for id,strlist in data.iteritems():
@@ -355,125 +285,6 @@ class VERSEVectorizer:
 			corpus.append(Counter(tokens))
 		return self.corpusToVectors(corpus,featureNamesOnly,"ngrams_betweenEntites",self.tfidf)
 		
-		
-	def doBigramsBetweenEntities(self,examples,featureNamesOnly):
-		corpus = []
-		for example in examples:
-			if len(example.arguments) != 2:
-				return None
-
-			sentenceid1,locs1 = example.arguments[0]
-			sentenceid2,locs2 = example.arguments[1]
-			if sentenceid1!=sentenceid2:
-				return None
-			
-			sentence = example.sentences[sentenceid1]
-			
-			if min(locs2) > max(locs1):
-				window = list(range(max(locs1)+1,min(locs2)))
-			else:
-				window = list(range(max(locs2)+1,min(locs1)))
-				
-			window = [ t for t in window if t >= 0 and t < len(sentence.tokens) ]
-			bigrams = [ (sentence.tokens[i].word.lower(),sentence.tokens[j].word.lower()) for i,j in zip(window,window[1:]) ]
-			corpus.append(Counter(bigrams))
-			
-		return self.corpusToVectors(corpus,featureNamesOnly,"ngrams_betweenEntites",self.tfidf)
-	
-	
-	def doBigramsEntityWindowLeft(self,examples,windowSize,argID,featureNamesOnly):
-		corpus = []
-		for example in examples:
-			sentenceid,locs = example.arguments[argID]
-			sentence = example.sentences[sentenceid]
-			
-			minL = min(locs)
-			leftWindow = list(range(minL-windowSize,minL))
-			leftWindow = [ t for t in leftWindow if t >= 0 and t < len(sentence.tokens) ]
-			bigrams = [ (sentence.tokens[i].word.lower(),sentence.tokens[j].word.lower()) for i,j in zip(leftWindow,leftWindow[1:]) ]
-			corpus.append(Counter(bigrams))
-		name = "Bigrams_EntityWindowLeft_%d_%d" % (windowSize,argID)
-		return self.corpusToVectors(corpus,featureNamesOnly,name,self.tfidf)
-		
-	def doBigramsEntityWindowRight(self,examples,windowSize,argID,featureNamesOnly):
-		corpus = []
-		for example in examples:
-			sentenceid,locs = example.arguments[argID]
-			sentence = example.sentences[sentenceid]
-			
-			maxL = max(locs)
-			rightWindow = list(range(maxL+1,maxL+windowSize+1))
-			rightWindow = [ t for t in rightWindow if t >= 0 and t < len(sentence.tokens) ]
-			bigrams = [ (sentence.tokens[i].word.lower(),sentence.tokens[j].word.lower()) for i,j in zip(rightWindow,rightWindow[1:]) ]
-			corpus.append(Counter(bigrams))
-			
-		name = "Bigrams_EntityWindowRight_%d_%d" % (windowSize,argID)
-		return self.corpusToVectors(corpus,featureNamesOnly,name,self.tfidf)
-		
-	def doBigramsEntityWindowBoth(self,examples,windowSize,argID,featureNamesOnly):
-		corpus = []
-		for example in examples:
-			sentenceid,locs = example.arguments[argID]
-			sentence = example.sentences[sentenceid]
-			
-			minL,maxL = min(locs),max(locs)
-			leftWindow = list(range(minL-windowSize,minL))
-			rightWindow = list(range(maxL+1,maxL+windowSize+1))
-			bothWindows = leftWindow + rightWindow
-			bothWindows = [ t for t in bothWindows if t >= 0 and t < len(sentence.tokens) ]
-			bigrams = [ (sentence.tokens[i].word.lower(),sentence.tokens[j].word.lower()) for i,j in zip(bothWindows,bothWindows[1:]) ]
-			corpus.append(Counter(bigrams))
-			
-		name = "Bigrams_EntityWindowBoth_%d_%d" % (windowSize,argID)
-		return self.corpusToVectors(corpus,featureNamesOnly,name,self.tfidf)
-	
-	def doNGramsEntityWindowLeft(self,examples,windowSize,argID,featureNamesOnly):
-		corpus = []
-		for example in examples:
-			sentenceid,locs = example.arguments[argID]
-			sentence = example.sentences[sentenceid]
-			
-			minL = min(locs)
-			leftWindow = list(range(minL-windowSize,minL))
-			leftWindow = [ t for t in leftWindow if t >= 0 and t < len(sentence.tokens) ]
-			tokens = [ sentence.tokens[t].word.lower() for t in leftWindow ]
-			
-			corpus.append(Counter(tokens))
-		name = "Ngrams_EntityWindowLeft_%d_%d" % (windowSize,argID)
-		return self.corpusToVectors(corpus,featureNamesOnly,name,self.tfidf)
-		
-	def doNGramsEntityWindowRight(self,examples,windowSize,argID,featureNamesOnly):
-		corpus = []
-		for example in examples:
-			sentenceid,locs = example.arguments[argID]
-			sentence = example.sentences[sentenceid]
-			
-			maxL = max(locs)
-			rightWindow = list(range(maxL+1,maxL+windowSize+1))
-			rightWindow = [ t for t in rightWindow if t >= 0 and t < len(sentence.tokens) ]
-			tokens = [ sentence.tokens[t].word.lower() for t in rightWindow ]
-			
-			corpus.append(Counter(tokens))
-		name = "Ngrams_EntityWindowRight_%d_%d" % (windowSize,argID)
-		return self.corpusToVectors(corpus,featureNamesOnly,name,self.tfidf)
-		
-	def doNGramsEntityWindowBoth(self,examples,windowSize,argID,featureNamesOnly):
-		corpus = []
-		for example in examples:
-			sentenceid,locs = example.arguments[argID]
-			sentence = example.sentences[sentenceid]
-			
-			minL,maxL = min(locs),max(locs)
-			leftWindow = list(range(minL-windowSize,minL))
-			rightWindow = list(range(maxL+1,maxL+windowSize+1))
-			bothWindows = leftWindow + rightWindow
-			bothWindows = [ t for t in bothWindows if t >= 0 and t < len(sentence.tokens) ]
-			tokens = [ sentence.tokens[t].word.lower() for t in bothWindows ]
-			
-			corpus.append(Counter(tokens))
-		name = "Ngrams_EntityWindowBoth_%d_%d" % (windowSize,argID)
-		return self.corpusToVectors(corpus,featureNamesOnly,name,self.tfidf)
-
 	def doSelectedTokenTypes(self,examples,argID,featureNamesOnly):
 		corpus = []
 
@@ -489,102 +300,6 @@ class VERSEVectorizer:
 			corpus.append(Counter([type]))
 		return self.corpusToVectors(corpus,featureNamesOnly,'SelectedTokenTypes_'+str(argID),False)
 
-	def doTypesNearSelectedTokens(self,examples,distance):
-		corpus = []
-
-		for example in examples:
-			# Get all the neighbours of all the selected tokens
-			selectedTokens = sum(example.argTokenGroups,[])
-			neighbours = sum ([[ t-distance,t+distance ] for t in selectedTokens ], [])
-			# Get all the types of these tokens
-			types = [ example.knownTriggerTypesByLoc[n] for n in neighbours if n in example.knownTriggerTypesByLoc ]
-
-			# Convert the list of types to a dictionary of counts for use by the vectorizer
-			corpus.append(Counter(types))
-		return self.corpusToVectors(corpus,featureNamesOnly,'TypesNearSelectedTokens',False)
-		
-	def doTypesNearSelectedTokensWithDependencies(self,examples,distance):
-		corpus = []
-
-		for example in examples:
-			if distance == 1:
-				neighbourList = example.dep_neighbours
-			elif distance == 2:
-				neighbourList = example.dep_neighbours2
-			else:
-				raise RuntimeError("Expecting distance to be 1 or 2")
-
-			# Get all the neighbours of all the selected tokens
-			selectedTokens = sum(example.argTokenGroups,[])
-			neighbours = sum ([ list(neighbourList[t]) for t in selectedTokens ], [])
-			# Get all the types of these tokens
-			types = [ example.knownTriggerTypesByLoc[n] for n in neighbours if n in example.knownTriggerTypesByLoc ]
-
-			# Convert the list of types to a dictionary of counts for use by the vectorizer
-			corpus.append(Counter(types))
-		return self.corpusToVectors(corpus,featureNamesOnly,'TypesNearSelectedTokensWithDependencies',False)
-
-	def doLemmasOfArguments(self,examples,argID,featureNamesOnly):
-		corpus = []
-		for example in examples:
-			sentenceid,locs = example.arguments[argID]
-			sentence = example.sentences[sentenceid]
-			tokens = [ sentence.tokens[t].lemma.lower() for t in locs ]
-			corpus.append(Counter(tokens))
-		return self.corpusToVectors(corpus,featureNamesOnly,'LemmasOfArguments_'+str(argID),self.tfidf)
-		
-	def doNGramsOfArguments(self,examples,argID,featureNamesOnly):
-		corpus = []
-		for example in examples:
-			sentenceid,locs = example.arguments[argID]
-			sentence = example.sentences[sentenceid]
-			tokens = [ sentence.tokens[t].word.lower() for t in locs ]
-			corpus.append(Counter(tokens))
-		return self.corpusToVectors(corpus,featureNamesOnly,'NGramsOfArguments_'+str(argID),self.tfidf)
-	
-	def doNGramsOfArgumentsWithPOS(self,examples,argID,featureNamesOnly):
-		corpus = []
-		for example in examples:
-			sentenceid,locs = example.arguments[argID]
-			sentence = example.sentences[sentenceid]
-			tokensWithPOS = [ (sentence.tokens[t].word.lower(),sentence.tokens[t].partofspeech) for t in locs ]
-			corpus.append(Counter(tokensWithPOS))
-		return self.corpusToVectors(corpus,featureNamesOnly,'NGramsOfArgumentsWithPOS_'+str(argID),self.tfidf)	
-
-	def doNGrams(self,examples,featureNamesOnly):
-		corpus = []
-		for example in examples:
-			allTokens = []
-			for sentenceid,_ in example.arguments:
-				sentence = example.sentences[sentenceid]
-				tokens = [ t.word.lower() for t in sentence.tokens ]
-				allTokens = allTokens + tokens
-			corpus.append(Counter(allTokens))
-		return self.corpusToVectors(corpus,featureNamesOnly, 'NGrams', self.tfidf)
-
-	def doLemmas(self,examples,featureNamesOnly):
-		corpus = []
-		for example in examples:
-			allTokens = []
-			for sentenceid,_ in example.arguments:
-				sentence = example.sentences[sentenceid]
-				tokens = [ t.lemma.lower() for t in sentence.tokens ]
-				allTokens = allTokens + tokens
-			corpus.append(Counter(allTokens))
-		return self.corpusToVectors(corpus,featureNamesOnly, 'Lemmas', self.tfidf)
-
-
-	def doNGramsWithPOS(self,examples,featureNamesOnly):
-		corpus = []
-		for example in examples:
-			allTokensWithPOS = []
-			for sentenceid,_ in example.arguments:
-				sentence = example.sentences[sentenceid]
-				tokensWithPOS = [ (t.word.lower(),t.partofspeech) for t in sentence.tokens ]
-				allTokensWithPOS = allTokensWithPOS + tokensWithPOS
-			corpus.append(Counter(allTokensWithPOS))
-		return self.corpusToVectors(corpus,featureNamesOnly, 'NGramsWithPOS', self.tfidf)
-
 		
 	def doBiGrams(self,examples,featureNamesOnly):
 		corpus = []
@@ -598,36 +313,6 @@ class VERSEVectorizer:
 			corpus.append(Counter(allBigrams))
 		return self.corpusToVectors(corpus,featureNamesOnly, 'BiGrams', self.tfidf)
 		
-	def doSkipGrams(self,examples,gap,featureNamesOnly):
-		corpus = []
-		for example in examples:
-			allSkipgrams = []
-			for sentenceid,_ in example.arguments:
-				sentence = example.sentences[sentenceid]
-				skipgrams = [ (sentence.tokens[i].word.lower(),sentence.tokens[i+gap].word.lower()) for i in range(len(sentence.tokens)-gap) ]
-				allSkipgrams = allSkipgrams + skipgrams
-			
-			corpus.append(Counter(allSkipgrams))
-		return self.corpusToVectors(corpus,featureNamesOnly, 'SkipGrams_%d' % gap, self.tfidf)
-	
-	def doSplitAcrossSentences(self,examples,featureNamesOnly):
-		if featureNamesOnly:
-			return ["splitAcrossSentences"]
-	
-		out = lil_matrix((len(examples),1))
-		for i,example in enumerate(examples):
-			sentenceid0,_ = example.arguments[0]
-			sameSentence = True
-			for thisSID,_ in example.arguments:
-				if sentenceid0 != thisSID:
-					sameSentence = False
-					break
-					
-			if not sameSentence:
-				out[i,0] = 1
-					
-		return coo_matrix(out)
-	
 	def doDependencyPathNearSelected(self,examples,argID,featureNamesOnly):
 		corpus = []
 		for example in examples:
@@ -700,73 +385,6 @@ class VERSEVectorizer:
 	
 		return self.corpusToVectors(corpus,featureNamesOnly, 'DependencyPathElements', False)
 
-	def doNGramsOfDependencyPath(self,examples,featureNamesOnly):
-		corpus = []
-		for example in examples:
-			sentenceid0,_ = example.arguments[0]
-			sameSentence = True
-			for thisSID,_ in example.arguments:
-				if sentenceid0 != thisSID:
-					sameSentence = False
-		
-			# Force single argument examples to use a path from enttity to root
-			if len(example.arguments) == 1:
-				sameSentence = False
-
-			if sameSentence:
-				sentence = example.sentences[sentenceid0]
-				locs = [ l for _,l in example.arguments ]
-				locs = sum(locs, [])
-				
-				nodes,_ = sentence.extractMinSubgraphContainingNodes(locs)
-
-				ngrams = [ sentence.tokens[a].word.lower() for a in nodes ]
-			else:
-				ngrams = []
-				for sentenceid,locs in example.arguments:
-					sentence = example.sentences[sentenceid]
-					nodes,_ = sentence.extractSubgraphToRoot(locs)
-					tmpNgrams = [ sentence.tokens[a].word.lower() for a in nodes ]
-					ngrams += tmpNgrams
-			
-			corpus.append(Counter(ngrams))
-			
-		return self.corpusToVectors(corpus,featureNamesOnly, 'NGramsOfDependencyPath', self.tfidf)
-		
-	def doBiGramsOfDependencyPath(self,examples,featureNamesOnly):
-		corpus = []
-		for example in examples:
-			sentenceid0,_ = example.arguments[0]
-			sameSentence = True
-			for thisSID,_ in example.arguments:
-				if sentenceid0 != thisSID:
-					sameSentence = False
-			
-			# Force single argument examples to use a path from enttity to root
-			if len(example.arguments) == 1:
-				sameSentence = False
-
-			if sameSentence:
-				sentence = example.sentences[sentenceid0]
-				
-				locs = [ l for _,l in example.arguments ]
-				locs = sum(locs, [])
-				
-				_,edges = sentence.extractMinSubgraphContainingNodes(locs)
-
-				bigrams = [ (sentence.tokens[a].word.lower(),sentence.tokens[b].word.lower()) for a,b in edges ]
-			else:
-				bigrams = []
-				for sentenceid,locs in example.arguments:
-					sentence = example.sentences[sentenceid]
-					_,edges = sentence.extractSubgraphToRoot(locs)
-					tmpBigrams = [ (sentence.tokens[a].word.lower(),sentence.tokens[b].word.lower()) for a,b in edges ]
-					bigrams += tmpBigrams
-			
-			corpus.append(Counter(bigrams))
-			
-		return self.corpusToVectors(corpus,featureNamesOnly, 'BiGramsOfDependencyPath', self.tfidf)
-		
 	def getTrainingVectors(self):
 		return self.trainingVectors
 		
@@ -775,18 +393,8 @@ class VERSEVectorizer:
 		#for c in candidates:
 		#	assert isinstance(c,kindred.CandidateRelation)
 		
-		options = ["ngrams","selectedngrams","bigrams","ngramsPOS","selectedngramsPOS","ngramsOfDependencyPath","bigramsOfDependencyPath","selectedTokenTypes","lemmas","selectedlemmas","dependencyPathElements","dependencyPathNearSelected","splitAcrossSentences","skipgrams_2","skipgrams_3","skipgrams_4","skipgrams_5","skipgrams_6","skipgrams_7","skipgrams_8","skipgrams_9","skipgrams_10","ngrams_betweenEntities","bigrams_betweenEntities"]
-		
-		for i in range(1,10):
-			options.append("ngrams_entityWindowLeft_%d" % i)
-			options.append("ngrams_entityWindowRight_%d" % i)
-			options.append("ngrams_entityWindowBoth_%d" % i)
-			options.append("bigrams_entityWindowBoth_%d" % i)
-			
-		for i in range(2,10):
-			options.append("bigrams_entityWindowLeft_%d" % i)
-			options.append("bigrams_entityWindowRight_%d" % i)
-		
+		options = ["selectedTokenTypes","ngrams_betweenEntities","bigrams","dependencyPathElements","dependencyPathNearSelected"]
+
 		if featureChoice is None:
 			#self.chosenFeatures = ["selectedTokenTypes","dependencyPathElements"]
 			self.chosenFeatures = ["selectedTokenTypes"]
