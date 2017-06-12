53c5d8cb8d479c0a7f698703e7ee9b2dff3e89dc
Jake Lever <jlever@bcgsc.ca>
2017-06-12 16:52:27 -0700

    Removed unused code

--- kindred/Vectorizer.py
+++ kindred/Vectorizer.py
@@ -9,39 +9,8 @@ from kindred.VERSE_vectorizer import VERSEVectorizer
 
 class Vectorizer:
 	def __init__(self):
-		self.dictVectorizers = {}
 		self.verseVectorizer = None
 		pass
-		
-	def corpusToVectors(self,corpus,name,doTFIDF=True):
-		vectorizerName = "%s_vectorizer" %  name
-		tfidfName = "%s_tfidf" % name
-		
-		if not vectorizerName in self.dictVectorizers:
-			self.dictVectorizers[vectorizerName] = DictVectorizer()
-			vecs = self.dictVectorizers[vectorizerName].fit_transform(corpus)
-			if doTFIDF:
-				self.dictVectorizers[tfidfName] = TfidfTransformer()
-				vecs = self.dictVectorizers[tfidfName].fit_transform(vecs)
-				
-			return vecs
-		else:
-			vecs = self.dictVectorizers[vectorizerName].transform(corpus)
-			if doTFIDF:
-				vecs = self.dictVectorizers[tfidfName].transform(vecs)
-			return vecs
-		
-	def doSelectedTokenTypes(self,candidateRelations,argID):
-		corpus = []
-
-		for candidate in candidateRelations:
-			if argID < len(candidate.entitiesInRelation):
-				entityID = candidate.entitiesInRelation[argID]
-				assert entityID in candidate.processedSentence.getEntityIDs()
-				entityType = candidate.processedSentence.getEntityType(entityID)
-				corpus.append(Counter([entityType]))
-			
-		return self.corpusToVectors(corpus,'SelectedTokenTypes_'+str(argID),False)
 		
 	def transform(self,corpus,candidateRelations,featureChoice=None,tfidf=None):
 		assert isinstance(corpus,kindred.Corpus)
