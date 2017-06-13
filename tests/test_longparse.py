8e47a0e9ac72d5ae6ed0d399a1c2ab38fd3e2f68
Jake Lever <jlever@bcgsc.ca>
2017-06-13 09:05:57 -0700

    Added long parse test

---
+++ tests/test_longparse.py
@@ -0,0 +1,35 @@
+
+import kindred
+from kindred.RelationClassifier import RelationClassifier
+
+from kindred.datageneration import generateData,generateTestData
+
+def test_longParse():
+	trainCorpus, testCorpusGold = generateTestData(positiveCount=100,negativeCount=100)
+	parser = kindred.Parser()
+	print("A")
+	parser.parse(trainCorpus)
+	print("B")
+	parser.parse(testCorpusGold)
+
+	#trainCorpus, testCorpusGold = generateTestData(positiveCount=100,negativeCount=100)
+	#parser = kindred.Parser()
+	parser.parse(trainCorpus)
+	print("C")
+	parser.parse(testCorpusGold)
+	print("D")
+
+	trainCorpus, devCorpus = generateTestData(positiveCount=100,negativeCount=100,relTypes=2)
+	#parser = kindred.Parser()
+	parser.parse(trainCorpus)
+	print("E")
+	parser.parse(devCorpus)
+	print("F")
+
+	#trainCorpus, devCorpus = generateTestData(positiveCount=100,negativeCount=100,relTypes=2)
+	#parser = kindred.Parser()
+	parser.parse(trainCorpus)
+	print("G")
+	parser.parse(devCorpus)
+	print("H")
+
