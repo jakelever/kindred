9f6f1936f0e0e5dc53e9207fdaff54b9b27f11cf
Jake Lever <jlever@bcgsc.ca>
2017-07-21 17:25:42 -0700

    Added extra testing for TFIDF in vectorizer

--- tests/test_vectorizer.py
+++ tests/test_vectorizer.py
@@ -67,7 +67,6 @@ def test_vectorizer_ngrams_betweenEntities():
 	matrix2 = vectorizer.transform(corpus2)
 			
 	colnames = vectorizer.getFeatureNames()
-	print colnames
 	expectedNames = [u'ngrams_betweenentities_a', u'ngrams_betweenentities_be', u'ngrams_betweenentities_can', u'ngrams_betweenentities_clinical', u'ngrams_betweenentities_common', u'ngrams_betweenentities_effect', u'ngrams_betweenentities_failed', u'ngrams_betweenentities_for', u'ngrams_betweenentities_is', u'ngrams_betweenentities_known', u'ngrams_betweenentities_of', u'ngrams_betweenentities_side', u'ngrams_betweenentities_treated', u'ngrams_betweenentities_treatment', u'ngrams_betweenentities_trials', u'ngrams_betweenentities_with']
 	assert colnames == expectedNames
 	
@@ -150,6 +149,147 @@ def test_vectorizer_dependencyPathNearSelected():
 
 	chosenFeatures = ["dependencyPathNearSelected"]
 	vectorizer = kindred.Vectorizer(featureChoice=chosenFeatures,tfidf=True)
+	
+	matrix1 = vectorizer.fit_transform(corpus1)
+	matrix2 = vectorizer.transform(corpus2)
+	
+	colnames = vectorizer.getFeatureNames()
+	expectedNames = ['dependencypathnearselectedtoken_0_nsubj', 'dependencypathnearselectedtoken_0_nsubjpass', 'dependencypathnearselectedtoken_1_nsubj', 'dependencypathnearselectedtoken_1_nsubjpass']
+	assert colnames == expectedNames
+	
+	# As a quick check, we'll confirm that the column means are as expected
+	colmeans1 = np.sum(matrix1,axis=0)
+	assert colmeans1.tolist() == [[ 3, 1, 3, 1 ]]
+	
+	# As a quick check, we'll confirm that the column means are as expected
+	colmeans2 = np.sum(matrix2,axis=0)
+	assert colmeans2.tolist() == [[ 7, 1, 7, 1 ]]
+	
+
+def test_vectorizer_selectedTokenTypes_noTFIDF():
+	corpus1, _ = generateTestData(positiveCount=5,negativeCount=5)
+	corpus2, _ = generateTestData(positiveCount=10,negativeCount=10)
+
+	candidateBuilder = kindred.CandidateBuilder()
+	candidateBuilder.fit_transform(corpus1)
+	candidateBuilder.transform(corpus2)
+
+	candidates = corpus1.getCandidateRelations()
+
+	chosenFeatures = ["selectedTokenTypes"]
+	vectorizer = kindred.Vectorizer(featureChoice=chosenFeatures,tfidf=False)
+	
+	matrix1 = vectorizer.fit_transform(corpus1)
+	matrix2 = vectorizer.transform(corpus2)
+		
+	colnames = vectorizer.getFeatureNames()
+	expectedNames = ['selectedtokentypes_0_disease', 'selectedtokentypes_0_disease2', 'selectedtokentypes_0_drug', 'selectedtokentypes_1_disease', 'selectedtokentypes_1_disease2', 'selectedtokentypes_1_drug']
+	assert colnames == expectedNames
+	
+	# As a quick check, we'll confirm that the column means are as expected
+	colmeans1 = np.sum(matrix1,axis=0)
+	assert colmeans1.tolist() == [[ 2,2,4, 2,2,4 ]]
+	
+	colmeans2 = np.sum(matrix2,axis=0)
+	assert colmeans2.tolist() == [[ 5,4,9, 5,4,9 ]]
+	
+def test_vectorizer_ngrams_betweenEntities_noTFIDF():
+	corpus1, _ = generateTestData(positiveCount=5,negativeCount=5)
+	corpus2, _ = generateTestData(positiveCount=10,negativeCount=10)
+
+	candidateBuilder = kindred.CandidateBuilder()
+	candidateBuilder.fit_transform(corpus1)
+	candidateBuilder.transform(corpus2)
+
+	chosenFeatures = ["ngrams_betweenEntities"]
+	vectorizer = kindred.Vectorizer(featureChoice=chosenFeatures,tfidf=False)
+	
+	matrix1 = vectorizer.fit_transform(corpus1)
+	matrix2 = vectorizer.transform(corpus2)
+			
+	colnames = vectorizer.getFeatureNames()
+	expectedNames = [u'ngrams_betweenentities_a', u'ngrams_betweenentities_be', u'ngrams_betweenentities_can', u'ngrams_betweenentities_clinical', u'ngrams_betweenentities_common', u'ngrams_betweenentities_effect', u'ngrams_betweenentities_failed', u'ngrams_betweenentities_for', u'ngrams_betweenentities_is', u'ngrams_betweenentities_known', u'ngrams_betweenentities_of', u'ngrams_betweenentities_side', u'ngrams_betweenentities_treated', u'ngrams_betweenentities_treatment', u'ngrams_betweenentities_trials', u'ngrams_betweenentities_with']
+	assert colnames == expectedNames
+	
+	# As a quick check, we'll confirm that the column means are as expected
+	expected1 = [4.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 4.0, 4.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0]
+	colmeans1 = np.sum(matrix1,axis=0).tolist()[0]
+	for gotVal,expectedVal in zip(colmeans1,expected1):
+		assert round(gotVal,8) == round(expectedVal,8) # Check rounded values (for floating point comparison issue)
+		
+	expected2 = [4.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 4.0, 4.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0]
+	colmeans2 = np.sum(matrix1,axis=0).tolist()[0]
+	for gotVal,expectedVal in zip(colmeans2,expected2):
+		assert round(gotVal,8) == round(expectedVal,8) # Check rounded values (for floating point comparison issue)
+	
+def test_vectorizer_bigrams_noTFIDF():
+	corpus1, _ = generateTestData(positiveCount=5,negativeCount=5)
+	corpus2, _ = generateTestData(positiveCount=10,negativeCount=10)
+
+	candidateBuilder = kindred.CandidateBuilder()
+	candidateBuilder.fit_transform(corpus1)
+	candidateBuilder.transform(corpus2)
+
+	chosenFeatures = ["bigrams"]
+	vectorizer = kindred.Vectorizer(featureChoice=chosenFeatures,tfidf=False)
+	
+	matrix1 = vectorizer.fit_transform(corpus1)
+	matrix2 = vectorizer.transform(corpus2)
+	
+	colnames = vectorizer.getFeatureNames()
+	expectedNames = [u'bigrams_a_common', u'bigrams_a_known', u'bigrams_be_treated', u'bigrams_bmzvpvwbpw_failed', u'bigrams_can_be', u'bigrams_clinical_trials', u'bigrams_common_treatment', u'bigrams_effect_of', u'bigrams_failed_clinical', u'bigrams_for_kyekjnkrfo', u'bigrams_for_zgwivlcmly', u'bigrams_gnorcyvmer_is', u'bigrams_is_a', u'bigrams_known_side', u'bigrams_kyekjnkrfo_.', u'bigrams_of_ruswdgzajr', u'bigrams_ootopaoxbg_can', u'bigrams_pehhjnlvvewbjccovflf_is', u'bigrams_ruswdgzajr_.', u'bigrams_side_effect', u'bigrams_treated_with', u'bigrams_treatment_for', u'bigrams_trials_for', u'bigrams_vgypkemhjr_.', u'bigrams_with_vgypkemhjr', u'bigrams_zgwivlcmly_.']
+	assert colnames == expectedNames
+	
+	# As a quick check, we'll confirm that the column means are as expected
+	expected1 = [4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 8.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0]
+	colmeans1 = np.sum(matrix1,axis=0).tolist()[0]
+	for gotVal,expectedVal in zip(colmeans1,expected1):
+		assert round(gotVal,8) == round(expectedVal,8) # Check rounded values (for floating point comparison issue)
+		
+	# As a quick check, we'll confirm that the column means are as expected
+	expected2 = [4.0, 0.0, 4.0, 4.0, 4.0, 8.0, 4.0, 0.0, 8.0, 0.0, 0.0, 0.0, 4.0, 0.0, 4.0, 0.0, 4.0, 4.0, 0.0, 0.0, 4.0, 4.0, 8.0, 4.0, 4.0, 0.0]
+	colmeans2 = np.sum(matrix2,axis=0).tolist()[0]
+	for gotVal,expectedVal in zip(colmeans2,expected2):
+		assert round(gotVal,8) == round(expectedVal,8) # Check rounded values (for floating point comparison issue)
+		
+	
+def test_vectorizer_dependencyPathElements_noTFIDF():
+	corpus1, _ = generateTestData(positiveCount=5,negativeCount=5)
+	corpus2, _ = generateTestData(positiveCount=10,negativeCount=10)
+
+	candidateBuilder = kindred.CandidateBuilder()
+	candidateBuilder.fit_transform(corpus1)
+	candidateBuilder.transform(corpus2)
+
+	chosenFeatures = ["dependencyPathElements"]
+	vectorizer = kindred.Vectorizer(featureChoice=chosenFeatures,tfidf=False)
+	
+	matrix1 = vectorizer.fit_transform(corpus1)
+	matrix2 = vectorizer.transform(corpus2)
+	
+	colnames = vectorizer.getFeatureNames()
+	expectedNames = [u'dependencypathelements_nmod', u'dependencypathelements_nsubj', u'dependencypathelements_nsubjpass', u'dependencypathelements_punct']
+	assert colnames == expectedNames
+	
+	# As a quick check, we'll confirm that the column means are as expected
+	colmeans1 = np.sum(matrix1,axis=0)
+	assert colmeans1.tolist() == [[ 8, 6, 2, 4 ]]
+	
+	# As a quick check, we'll confirm that the column means are as expected
+	colmeans2 = np.sum(matrix2,axis=0)
+	assert colmeans2.tolist() == [[ 10, 14, 2, 6 ]]
+	
+	
+def test_vectorizer_dependencyPathNearSelected_noTFIDF():
+	corpus1, _ = generateTestData(positiveCount=5,negativeCount=5)
+	corpus2, _ = generateTestData(positiveCount=10,negativeCount=10)
+
+	candidateBuilder = kindred.CandidateBuilder()
+	candidateBuilder.fit_transform(corpus1)
+	candidateBuilder.transform(corpus2)
+
+	chosenFeatures = ["dependencyPathNearSelected"]
+	vectorizer = kindred.Vectorizer(featureChoice=chosenFeatures,tfidf=False)
 	
 	matrix1 = vectorizer.fit_transform(corpus1)
 	matrix2 = vectorizer.transform(corpus2)
