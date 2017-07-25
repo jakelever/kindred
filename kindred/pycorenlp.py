675d793709d24ea007b1cc2ebbe02192f78b9ff5
Jake Lever <jlever@bcgsc.ca>
2017-07-25 16:15:41 -0700

    More minimalising pycorenlp

--- kindred/pycorenlp.py
+++ kindred/pycorenlp.py
@@ -6,21 +6,11 @@ import json, requests
 class StanfordCoreNLP:
 
     def __init__(self, server_url):
-        if server_url[-1] == '/':
-            server_url = server_url[:-1]
         self.server_url = server_url
 
     def annotate(self, text, properties={}):
         assert isinstance(text, str) or isinstance(text,unicode)
         assert isinstance(properties, dict)
-
-        # Checks that the Stanford CoreNLP server is started.
-        try:
-            requests.get(self.server_url)
-        except requests.exceptions.ConnectionError:
-            raise Exception('Check whether you have started the CoreNLP server e.g.\n'
-            '$ cd stanford-corenlp-full-2015-12-09/ \n'
-            '$ java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer')
 
         data = text.encode('utf8')
         r = requests.post(
