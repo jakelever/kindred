0084dfe3c3d878d2575e113adfb9a537bf3c6135
Jake Lever <jlever@bcgsc.ca>
2017-07-26 13:16:13 -0700

    Add test to check the code in the README

---
+++ tests/test_checkREADME.py
@@ -0,0 +1,17 @@
+
+import os
+
+def test_README():
+	thisDir = os.path.dirname(os.path.realpath(__file__))
+	parentDir = os.path.dirname(thisDir)
+	readmePath = os.path.join(parentDir,'README.rst')
+
+	readmeTestPath = os.path.join(thisDir,'README.py')
+
+	with open(readmePath) as readmeF,open(readmeTestPath,'w') as testF:
+		for line in readmeF:
+			if line.startswith('>>> '):
+				testF.write(line[4:])
+
+	import README
+	
