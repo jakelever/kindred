614a7432e06f864059a371cc1cf53d25945da883
Jake Lever <jlever@bcgsc.ca>
2017-07-31 23:29:57 -0700

    Space to tabs to match rest of project

--- kindred/pycorenlp.py
+++ kindred/pycorenlp.py
@@ -6,21 +6,21 @@ import six
 
 class StanfordCoreNLP:
 
-    def __init__(self, server_url):
-        self.server_url = server_url
+	def __init__(self, server_url):
+		self.server_url = server_url
 
-    def annotate(self, text, properties={}):
+	def annotate(self, text, properties={}):
 		assert isinstance(text, six.string_types),"text must be a string"
-        assert isinstance(properties, dict)
+		assert isinstance(properties, dict)
 
-        data = text.encode('utf8')
-        r = requests.post(
-            self.server_url, params={
-                'properties': str(properties)
-            }, data=data, headers={'Connection': 'close'})
-        
-        assert 'outputFormat' in properties and properties['outputFormat'] == 'json'
-        output = json.loads(r.text, encoding='utf-8', strict=False)
+		data = text.encode('utf8')
+		r = requests.post(
+			self.server_url, params={
+				'properties': str(properties)
+			}, data=data, headers={'Connection': 'close'})
+		
+		assert 'outputFormat' in properties and properties['outputFormat'] == 'json'
+		output = json.loads(r.text, encoding='utf-8', strict=False)
 
-        return output
+		return output
 
