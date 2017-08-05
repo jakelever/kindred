b9d3fa7884e75ddd977e9ad9d9a2bd84a81dee97
Jake Lever <jlever@bcgsc.ca>
2017-08-05 12:36:28 -0700

    Adding MacOSX to Travis-CI

---
+++ .travis/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -exo pipefail
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+
+	# Install some custom requirements on OS X
+	brew update
+
+	case "${TOXENV}" in
+	py27)
+		# Install some custom Python 3.2 requirements on OS X
+		brew upgrade python;
+		pip2 install --upgrade virtualenv
+		virtualenv env2 -p python2
+		PS1=${PS1:=} source env2/bin/activate
+		;;
+	py36)
+		brew install python3;
+		pip3 install --upgrade virtualenv
+		virtualenv env3 -p python3
+		PS1=${PS1:=} source env3/bin/activate
+		;;
+	esac
+else
+	# Install some custom requirements on Linux
+	echo
+fi
+
+python --version
+
