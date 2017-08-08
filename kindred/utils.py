be6e53bda53239befdbb74fcc769855afb7cb309
Jake Lever <jlever@bcgsc.ca>
2017-08-07 17:04:25 -0700

    Use custom download code instead of wget

--- kindred/utils.py
+++ kindred/utils.py
@@ -2,6 +2,7 @@
 import kindred
 
 import socket
+import ssl
 import zipfile
 import hashlib
 import os
@@ -9,6 +10,7 @@ import sys
 import wget
 import shutil
 import six
+import requests
 
 if sys.version_info >= (3, 0):
 	import urllib.request
@@ -29,9 +31,22 @@ def _findDir(name, path):
 				return os.path.abspath(os.path.join(root, name))
 	return None
 
+# From: https://stackoverflow.com/questions/32763720/timeout-a-file-download-with-python-urllib
+def _downloadFile(url,filename,timeout=180):
+	# Make the actual request, set the timeout for no data to 10 seconds and enable streaming responses so we don't have to keep the large files in memory
+	request = requests.get(url, timeout=10, stream=True)
+
+	# Open the output file and make sure we write in binary mode
+	with open(filename, 'wb') as fh:
+		# Walk through the request response in chunks of 1024 * 1024 bytes, so 1MiB
+		for chunk in request.iter_content(1024 * 1024):
+			# Write the chunk to the file
+			fh.write(chunk)
+			# Optionally we can check here if the download is taking too long
+
 def _downloadFiles(files,downloadDirectory):
-	oldTimeout = socket.getdefaulttimeout()
-	socket.setdefaulttimeout(180)
+	#oldTimeout = socket.getdefaulttimeout()
+	#ssl.SSLSocket.settimeout(180)
 
 	if not os.path.isdir(downloadDirectory):
 		os.mkdir(downloadDirectory)
@@ -45,7 +60,8 @@ def _downloadFiles(files,downloadDirectory):
 				os.remove(downloadedPath)
 
 		if not os.path.isfile(downloadedPath):
-			wget.download(url,out=downloadedPath,bar=None)
+			#wget.download(url,out=downloadedPath,bar=None)
+			_downloadFile(url,downloadedPath)
 			
 			downloadedSHA256 = _calcSHA256(downloadedPath)
 			assert downloadedSHA256 == expectedSHA256, "SHA256 mismatch with downloaded file: %s" % shortName
@@ -55,4 +71,4 @@ def _downloadFiles(files,downloadDirectory):
 				zip_ref.extractall(path=downloadDirectory)
 				zip_ref.close()
 	
-	socket.setdefaulttimeout(oldTimeout)
+	#socket.setdefaulttimeout(oldTimeout)
