61b7effc56020783c88edca419df1f8d96eda6d2
Jake Lever <jlever@bcgsc.ca>
2017-08-22 19:51:59 -0700

    Added test to check evaluate stdout, stderr

--- tests/test_evaluate.py
+++ tests/test_evaluate.py
@@ -36,3 +36,30 @@ def test_evaluate():
 	with pytest.raises(RuntimeError) as excinfo:
 		kindred.evaluate(goldCorpus,testCorpus,metric='nonsense')
 	assert excinfo.value.args == ('Unknown metric: nonsense',)
+
+def test_evaluate_display(capfd):
+	goldText = 'The <disease id="T1">colorectal cancer</disease> was caused by mutations in <gene id="T2">APC</gene>. We also studied <disease id="T3">glioblastoma</disease>.'
+	goldText += '<relation type="typeA" subj="T2" obj="T1" />'
+	goldText += '<relation type="typeB" subj="T2" obj="T3" />'
+	goldText += '<relation type="typeA" subj="T3" obj="T1" />'
+	goldText += '<relation type="typeB" subj="T2" obj="T1" />'
+	goldText += '<relation type="typeC" subj="T2" obj="T1" />'
+
+	goldCorpus = kindred.Corpus(goldText)
+	
+	testCorpus = goldCorpus.clone()
+	testDoc = testCorpus.documents[0]
+	mapping = testDoc.getSourceEntityIDsToEntityIDs()
+	
+	# Remove a relation and add two different ones
+	testDoc.relations = testDoc.relations[:4]
+	testDoc.addRelation(kindred.Relation("typeX",entityIDs=[mapping["T1"],mapping["T2"]]))
+	testDoc.addRelation(kindred.Relation("typeX",entityIDs=[mapping["T1"],mapping["T3"]]))
+
+	_,_,_ = kindred.evaluate(goldCorpus,testCorpus,metric='all',display=True)
+	
+	out, err = capfd.readouterr()
+	expected = "typeA\tTP:2 FP:0 FN:0\tP:1.000000 R:1.000000 F1:1.000000\ntypeB\tTP:2 FP:0 FN:0\tP:1.000000 R:1.000000 F1:1.000000\ntypeC\tTP:0 FP:0 FN:1\tP:0.000000 R:0.000000 F1:0.000000\ntypeX\tTP:0 FP:2 FN:0\tP:0.000000 R:0.000000 F1:0.000000\n--------------------------------------------------\nAll  \tTP:4 FP:2 FN:1\tP:0.666667 R:0.800000 F1:0.727273\n"
+	assert out == expected
+	assert err == ""
+
