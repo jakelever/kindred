040e5b3c517628a6a2920b7d02e728082d12c829
Jake Lever <jlever@bcgsc.ca>
2017-12-29 20:11:54 -0800

    Update Save functions by removing separate predictedRelations parameter

--- kindred/saveFunctions.py
+++ kindred/saveFunctions.py
@@ -4,7 +4,7 @@ import codecs
 
 import kindred
 
-def saveDataFromSTFormat(data,predictedRelations,txtPath,a1Path,a2Path):
+def saveDocToSTFormat(data,txtPath,a1Path,a2Path):
 	assert isinstance(data,kindred.Document)
 
 	with codecs.open(txtPath,'w','utf8') as txtFile, codecs.open(a1Path,'w','utf8') as a1File, codecs.open(a2Path,'w','utf8') as a2File:
@@ -33,41 +33,17 @@ def saveDataFromSTFormat(data,predictedRelations,txtPath,a1Path,a2Path):
 			arguments = " ".join(["%s:%s" % (a,b) for a,b in zip(argNames,relationEntityIDs) ])
 			line = "R%d\t%s %s" % (i,relationType,arguments)
 			a2File.write(line+"\n")
-			
-		for i,r in enumerate(predictedRelations):
-			assert isinstance(r,kindred.Relation)
-			
-			relationType = r.relationType
-			inThisText = [ eID in entityIDsToSourceEntityIDs for eID in r.entityIDs ]
-			if not all(inThisText):
-				# Not all the entities in this relation are in this portion of text. So don't include
-				continue
-			
-			relationEntityIDs = [ entityIDsToSourceEntityIDs[eID] for eID in r.entityIDs ]
-			
-			if r.argNames is None:
-				argNames = [ ("arg%d" % (i+1)) for i in range(len(relationEntityIDs)) ]
-			else:
-				argNames = r.argNames
 
-			arguments = " ".join(["%s:%s" % (a,b) for a,b in zip(argNames,relationEntityIDs) ])
-			line = "R%d\t%s %s" % (i,relationType,arguments)
-			a2File.write(line+"\n")
-			
-
-
-def save(corpus,dataFormat,directory,predictedRelations=[]):
+def save(corpus,dataFormat,directory):
 	"""
 	Save a corpus to a directory
 	
 	:param corpus: The corpus of documents to save
 	:param dataFormat: Format of data to save (only 'standoff' is supported currently)
 	:param directory: Path to directory in which files should be saved
-	:param predictedRelations: Set of predicted relations to also save
 	:type corpus: kindred.Corpus
 	:type dataFormat: str
 	:type directory: str
-	:type predictedRelations: List of kindred.Relation
 	"""
 	
 	assert dataFormat == 'standoff'
@@ -86,6 +62,6 @@ def save(corpus,dataFormat,directory,predictedRelations=[]):
 			a1Path = os.path.join(directory,'%s.a1' % base)
 			a2Path = os.path.join(directory,'%s.a2' % base)
 
-			saveDataFromSTFormat(d,predictedRelations,txtPath,a1Path,a2Path)
+			saveDocToSTFormat(d,txtPath,a1Path,a2Path)
 
 	
--- tests/test_save.py
+++ tests/test_save.py
@@ -101,37 +101,6 @@ def test_saveStandoffFile_noArgNames():
 	
 	shutil.rmtree(tempDir)
 
-def test_saveStandoffFile_predictedRels_noArgNames():
-	text = "The colorectal cancer was caused by mutations in APC"
-	e1 = kindred.Entity(entityType="disease",text="colorectal cancer",position=[(4, 21)],sourceEntityID="T1")
-	e2 = kindred.Entity(entityType="gene",text="APC",position=[(49, 52)],sourceEntityID="T2")
-	doc = kindred.Document(text,[e1,e2])
-	corpus = kindred.Corpus()
-	corpus.addDocument(doc)
-
-	tempDir = tempfile.mkdtemp()
-
-	rel = kindred.Relation(relationType="causes",entityIDs=[e1.entityID,e2.entityID])
-	kindred.save(corpus,'standoff',tempDir,predictedRelations=[rel])
-
-	loadedCorpus = kindred.loadDir('standoff',tempDir)
-
-	assert isinstance(loadedCorpus,kindred.Corpus)
-	assert len(loadedCorpus.documents) == 1
-	loadedDoc = loadedCorpus.documents[0]
-	
-	assert isinstance(loadedDoc,kindred.Document)
-	entities = loadedDoc.getEntities()
-	relations = loadedDoc.getRelations()
-
-	sourceEntityIDsToEntityIDs = loadedDoc.getSourceEntityIDsToEntityIDs()
-
-	assertEntity(entities[0],expectedType='disease',expectedText='colorectal cancer',expectedPos=[(4,21)],expectedSourceEntityID="T1")
-	assertEntity(entities[1],expectedType='gene',expectedText='APC',expectedPos=[(49,52)],expectedSourceEntityID="T2")
-	assert relations == [kindred.Relation('causes',[sourceEntityIDsToEntityIDs["T1"],sourceEntityIDsToEntityIDs["T2"]],['arg1','arg2'])], "(%s) not as expected" % relations
-	
-	shutil.rmtree(tempDir)
-
 def test_saveBB3Data():
 	corpus = kindred.bionlpst.load('2016-BB3-event-train')
 	assert isinstance(corpus,kindred.Corpus)
@@ -180,51 +149,6 @@ def test_saveStandoffFile_SeparateSentences():
 	assert relations == [kindred.Relation('causes',[sourceEntityIDsToEntityIDs["T1"],sourceEntityIDsToEntityIDs["T2"]],['obj','subj'])], "(%s) not as expected" % relations
 	
 	shutil.rmtree(tempDir)
-	
-	
-	
-def test_saveStandoffFile_PredictedRelations():
-	texts = ['The <disease id="T1">colorectal cancer</disease> was caused by mutations in <gene id="T2">APC</gene><relation type="causes" subj="T2" obj="T1" />','<disease id="T1">Li-Fraumeni</disease> was caused by mutations in <gene id="T2">P53</gene><relation type="causes" subj="T2" obj="T1" />']
-	corpus = kindred.Corpus()
-	for t in texts:
-		doc = kindred.Document(t,loadFromSimpleTag=True)
-		corpus.addDocument(doc)
-	
-	predictedRelations = []
-	# Rip out the relations and save as predicted ones
-	for d in corpus.documents:
-		predictedRelations += d.relations
-		d.relations = []
-
-	tempDir = tempfile.mkdtemp()
-
-	kindred.save(corpus,'standoff',tempDir,predictedRelations=predictedRelations)
-
-	loadedCorpus = kindred.loadDir('standoff',tempDir)
-
-	assert isinstance(loadedCorpus,kindred.Corpus)
-	assert len(loadedCorpus.documents) == 2
-	
-	data = loadedCorpus.documents[0]
-	assert isinstance(data,kindred.Document)
-	entities = data.getEntities()
-	relations = data.getRelations()
-	sourceEntityIDsToEntityIDs = data.getSourceEntityIDsToEntityIDs()
-	assertEntity(entities[0],expectedType='disease',expectedText='colorectal cancer',expectedPos=[(4,21)],expectedSourceEntityID="T1")
-	assertEntity(entities[1],expectedType='gene',expectedText='APC',expectedPos=[(49,52)],expectedSourceEntityID="T2")
-	assert relations == [kindred.Relation('causes',[sourceEntityIDsToEntityIDs["T1"],sourceEntityIDsToEntityIDs["T2"]],['obj','subj'])], "(%s) not as expected" % relations
-	
-	data = loadedCorpus.documents[1]
-	assert isinstance(data,kindred.Document)
-	entities = data.getEntities()
-	relations = data.getRelations()
-	sourceEntityIDsToEntityIDs = data.getSourceEntityIDsToEntityIDs()
-	assertEntity(entities[0],expectedType='disease',expectedText='Li-Fraumeni',expectedPos=[(0,11)],expectedSourceEntityID="T1")
-	assertEntity(entities[1],expectedType='gene',expectedText='P53',expectedPos=[(39,42)],expectedSourceEntityID="T2")
-	assert relations == [kindred.Relation('causes',[sourceEntityIDsToEntityIDs["T1"],sourceEntityIDsToEntityIDs["T2"]],['obj','subj'])], "(%s) not as expected" % relations
-	
-	shutil.rmtree(tempDir)
-	
 	
 if __name__ == '__main__':
 	#test_saveStandoffFile_PredictedRelations()
