04591caff966e9e66b7bf0c16245e54e2b8bbd77
Jake Lever <jlever@bcgsc.ca>
2018-02-10 16:38:29 -0800

    Added extra download file test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -32,3 +32,21 @@ def test_corruptFileInWayOfDownload():
 	assert newSHA256 == fileInfo[2]
 
 	shutil.rmtree(tempDir)
+
+def test_downloadSubdir():
+	tempDir = tempfile.mkdtemp()
+	downloadDir = os.path.join(tempDir,'toBeCreated')
+
+	fileInfo = ('https://github.com/jakelever/kindred/archive/1.0.tar.gz','1.0.tar.gz','363d36269a6ea83e1a253a0709dc365c3536ecd2976096ea0600cd4ef2f1e33c')
+
+	expectedDownloadPath = os.path.join(downloadDir,fileInfo[1])
+
+	assert not os.path.isdir(downloadDir)
+	kindred.utils._downloadFiles([fileInfo],downloadDir)
+	assert os.path.isdir(downloadDir)
+
+	newSHA256 = hashlib.sha256(open(expectedDownloadPath, 'rb').read()).hexdigest()
+
+	assert newSHA256 == fileInfo[2]
+
+	shutil.rmtree(tempDir)
